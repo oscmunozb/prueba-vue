de5117d8784437b7bb9401d96e42f4f08cb7dbb0
Oscar <osmunozb@gmail.com>
2024-10-05 21:32:09 -0300

    firebase deploy

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCje2hjK_bA8NMbyLT3IcIrSluR9w99tuQ',
+  authDomain: 'prueba-vue-5b0ea.firebaseapp.com',
+  projectId: 'prueba-vue-5b0ea',
+  storageBucket: 'prueba-vue-5b0ea.appspot.com',
+  messagingSenderId: '807219839262',
+  appId: '1:807219839262:web:a88eb0d80f1aaed14bc90f'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
